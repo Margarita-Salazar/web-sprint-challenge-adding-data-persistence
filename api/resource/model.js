a26ba15649ac36d71414a8afb8a074e28d423f65
unknown <margarita.salazar.mls@gmail.com>
2021-06-11 16:45:28 -0500

    can get all resource, and create a resource

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,22 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+function getAllResources(){
+    return db('resources')
+}
+
+function getResourceById(id){
+    return db('resources')
+        .where('resource_id', id)
+        .first()
+}
+
+async function createResource(resource){
+    return db('resources')
+        .insert(resource, 'resource_id')
+        .then(([id]) => getResourceById(id))
+}
+
+module.exports = { 
+    getAllResources,
+    createResource
+}
