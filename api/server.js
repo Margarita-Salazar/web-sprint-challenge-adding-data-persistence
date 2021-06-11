2deb0404874d2b698fd208648d60cc397e843719
unknown <margarita.salazar.mls@gmail.com>
2021-06-11 02:30:59 -0500

    setup server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,11 @@
-// build your server here and require it from index.js
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('*', (req, res)=>{
+    res.send('<h1>Its working!!!</h1>')
+})
+
+module.exports = server
